5fc9aaaea12e398671d7e1d8c3a7ee51e9a4932d
austin7313 <austin7313@gmail.com>
2026-01-14 19:42:24 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,8 +5,6 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
-    order_id = Column(Integer, unique=True, index=True)  # optional for tracking
     customer_phone = Column(String, index=True)
     items = Column(String)
     amount = Column(Integer)
-    status = Column(String, default="Pending")
