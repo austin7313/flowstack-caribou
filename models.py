65b3fdacffef212baed8b28197e0c51f3d95f6a1
austin7313 <austin7313@gmail.com>
2026-01-14 20:25:06 +0300

    Update models.py

--- models.py
+++ models.py
@@ -8,11 +8,3 @@ class Order(Base):
     customer_phone = Column(String, index=True)
     items = Column(String)
     amount = Column(Integer)
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    phone = Column(String, unique=True, index=True)
-    stage = Column(String, default="start")
-    order_items = Column(String, default="")
