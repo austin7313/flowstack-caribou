f3df9ade3115adf89075c3d0afa8cb169a2b7f1f
austin7313 <austin7313@gmail.com>
2026-01-14 20:05:23 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://<user>:<password>@<host>:5432/<dbname>")
+# Replace this with your Supabase/Postgres URL
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://username:password@host:port/dbname")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
