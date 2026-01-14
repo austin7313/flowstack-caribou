f0715d4c7cb4e8f91360eb9801549ecae3d81eee
austin7313 <austin7313@gmail.com>
2026-01-14 19:19:13 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,12 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from datetime import datetime
+from sqlalchemy import Column, Integer, String
 from database import Base
 
 class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, unique=True, index=True)  # optional for tracking
     customer_phone = Column(String, index=True)
-    customer_name = Column(String, nullable=True)
     items = Column(String)
-    amount = Column(Float)
-    status = Column(String, default="awaiting_payment")
-    created_at = Column(DateTime, default=datetime.utcnow)
+    amount = Column(Integer)
+    status = Column(String, default="Pending")
